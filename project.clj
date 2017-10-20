a1be1691768ecb3c6359e7e3662bf7c335513021
ammazza <ammazza@users.noreply.github.com>
2017-10-20 11:05:30 +1100

    Plugin binary published on Clojars
    Project.clj is now configured to deploy binary to Clojars. README was updated with instructions for retrieving the plugin from Clojars.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject lein-deploy-uberjar "2.1.0"
+(defproject org.ammazza/lein-deploy-uberjar "2.1.0"
   :description "Deploy uberjars to Maven repositories, including private repositories hosted on S3."
   :url "https://github.com/ammazza/lein-deploy-uberjar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
+  :repositories {"clojars" {:url "https://clojars.org/repo" :sign-releases false}}
   :eval-in-leiningen true)
